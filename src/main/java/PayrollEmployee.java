0d5d10f4091114b4a440bec0ceeabd901c33a426
Gale King <grexkingiv@gmail.com>
2020-05-27 13:44:44 -0500

    Abstract class established

---
+++ src/main/java/PayrollEmployee.java
@@ -0,0 +1,36 @@
+public abstract  class PayrollEmployee {
+
+    int employeeId;
+    String firstName;
+    String lastName;
+    double paycheckTotal;
+
+    public PayrollEmployee(int employeeId, String firstName, String lastName, double paycheckTotal) {
+
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.paycheckTotal = paycheckTotal;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getPaycheckTotal() {
+        return paycheckTotal;
+    }
+
+    public double setPaycheckTotal(double newPaycheckTotal) {
+        this.paycheckTotal =newPaycheckTotal;
+        return newPaycheckTotal;
+    }
+}
